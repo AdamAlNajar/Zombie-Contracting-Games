ee3c2aba257633de0e4b02e69f40f1e87ba433f1
Adam Al Najar <119093751+AdamAlNajar@users.noreply.github.com>
2026-05-22 23:53:03 +0400

    Transport to cave working + 2
    Player sprite changes
    Basic cave structure working

--- Assets/Scripts/CaveTrigger.cs
+++ Assets/Scripts/CaveTrigger.cs
@@ -2,11 +2,14 @@ using UnityEngine;
 
 public class CaveTrigger : MonoBehaviour
 {
+    public GameObject transportPoint; // Point to transport player to
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
         {
             Debug.Log("Cave Entered"); // Transport player to cave
+            GameObject player = other.gameObject;
+            player.transform.position = transportPoint.transform.position;
         }
     }
     // Make Some sort of story to the cave
