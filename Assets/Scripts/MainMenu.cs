b1c87956babe8be34c453f87df0979e2b9ddd9b2
Adam Al Najar <119093751+AdamAlNajar@users.noreply.github.com>
2026-05-28 10:18:39 +0400

    Seperated Tutorial and Game +1

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -2,9 +2,13 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 public class MainMenu : MonoBehaviour
 {
-    public void Play()
+    public void PlayTutorial()
     {
         SceneManager.LoadScene("Prolog");
+    }
+    public void Play()
+    {
+        SceneManager.LoadScene("Game 1");
     }
     public void Quit()
     {
